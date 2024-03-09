3ec75a1d24d07c622acfc5e074226ff517b49bd9
Jagrit Pant <jagrit.pant2021@vitbhopal.ac.in>
2024-03-09 22:56:04 +0530

    updated code

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def index():
-    return render_template("index.html")
-
+def hello_world():
+    return "<p>Hello Azure Web Apps!</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
